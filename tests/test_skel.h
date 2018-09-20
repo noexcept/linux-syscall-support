0d81be2fcd30367cb401da198642d68b9fbe012f
Mike Frysinger <vapier@chromium.org>
2018-09-20 18:22:55 -0400

    add lseek/statfs tests
    Bug: chromium:219015
    Change-Id: Ifdc9e9f9703f3ece51ecb05727ff1040ecde5f20

--- tests/test_skel.h
+++ tests/test_skel.h
@@ -57,6 +57,58 @@
 
 #include "linux_syscall_support.h"
 
+/* Make sure the kernel doesn't overwrite more mem than we expect. */
+#define _PAD_POISON 0xba
+#define _PAD_SIZE 0x1000
+#define _PAD_TO_OBJ(buf) ((void *)((uintptr_t)(buf) + _PAD_SIZE))
+#define _PAD_FROM_OBJ(obj) ((void *)((uintptr_t)(obj) - _PAD_SIZE))
+
+void *_pad_buf_get(size_t obj_len) {
+  size_t len = obj_len + _PAD_SIZE * 2;
+  void *ret = malloc(len);
+  return _PAD_TO_OBJ(ret);
+}
+#define PAD_BUF_GET(obj) _pad_buf_get(sizeof(*obj))
+
+void _pad_buf_poison(void *buf, size_t obj_len) {
+  size_t len = obj_len + _PAD_SIZE * 2;
+  memset(buf, _PAD_POISON, len);
+}
+#define PAD_BUF_POISON(obj) _pad_buf_poison(_PAD_FROM_OBJ(obj), sizeof(*obj))
+
+void _pad_buf_check(void *_buf, size_t obj_len) {
+  size_t len = obj_len + _PAD_SIZE * 2;
+  size_t i;
+  unsigned char *buf = (unsigned char *)_buf;
+
+  for (i = 0; i < _PAD_SIZE; ++i)
+    if (buf[i] != _PAD_POISON)
+      goto err;
+  for (i = _PAD_SIZE + obj_len; i < len; ++i)
+    if (buf[i] != _PAD_POISON)
+      goto err;
+  return;
+
+ err:
+  fprintf(stderr, "buf[%#zx] = %#x (should have been %#x)\n",
+    i, buf[i], _PAD_POISON);
+  fprintf(stderr, "sizeof(obj) = %#zx; perhaps it should be larger?\n",
+    obj_len);
+  abort();
+}
+#define PAD_BUF_CHECK(obj) _pad_buf_check(_PAD_FROM_OBJ(obj), sizeof(*obj))
+
+#define PAD_BUF_FREE(obj) free(_PAD_FROM_OBJ(obj))
+
+void dump_buf(const void *buf, size_t obj_len) {
+  const uint8_t *u8 = (const uint8_t *)buf;
+  size_t i;
+
+  for (i = 0; i < obj_len; ++i)
+    printf("%02x ", u8[i]);
+  printf("\n");
+}
+
 void assert_buffers_eq_len(const void *buf1, const void *buf2, size_t len) {
   const uint8_t *u8_1 = (const uint8_t *)buf1;
   const uint8_t *u8_2 = (const uint8_t *)buf2;
